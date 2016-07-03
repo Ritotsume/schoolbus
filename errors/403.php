aef6f58c438d31c7c5818f381c37b06e157d8d7f
Ritotsume <gianfrancesco@outlook.com>
2016-07-03 12:38:52 -0300

    add página de erro 403

---
+++ errors/403.php
@@ -0,0 +1,29 @@
+<div class="side-body">
+  <div class="page-title">
+    <span class="title">403 - Proibido</span>
+    <div class="description">O conteúdo não pode ser acessado.</div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="card">
+        <div class="card-header">
+          <div class="card-title">
+            <div class="title">Desculpe. Mas o conteúdo que você procura possui acesso restrito.</div>
+          </div>
+        </div>
+        <div class="card-body">
+          <h4>Siga esses passos para tentar solucionar o problema:</h4>
+          <ul>
+            <li>Tente acessar novamente o conteúdo desejado.</li>
+            <li>Efetue login novamente e então tente acessar o conteúdo desejado.</li>
+            <li>Anote a URL ou caminho utilizado que levou a esta página.</li>
+            <li>
+              Caso o erro persista, entre em contato com o suporte. Lembre-se de emcaminhar as informações
+              obtidas no passo anterior, isso facilitará a solução do problema.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>

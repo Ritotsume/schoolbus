eb12bbcdacc02e40d299b39366a66a6982833479
Ritotsume <gianfrancesco@outlook.com>
2016-07-03 21:56:53 -0300

    correção no detalhamento de rotas

--- content/views/rota/detail.php
+++ content/views/rota/detail.php
@@ -22,121 +22,124 @@
         </div>
         <div class="card-body">
 
-          <div class="form-group">
-            <label for="rota_inicio" class="col-md-3 control-label">Início do trajeto</label>
-            <div class="col-md-4">
-              <?php
-              $rotaInicio = new ModelEnderecos;
-              $inicio = $rotaInicio->getLogradouro($dataRota[0]['rota_saida']);
-              $inicio = $inicio[0]['logradouro_nome'] . ', ' . $inicio[0]['bairros_nome'] . ', ' . $inicio[0]['cidade_nome'] . ' - ' . $inicio[0]['cidade_uf'];
-              echo $inicio;
-              ?>
+          <form id="detail-rotas" method="post" action="">
+
+            <div class="form-group">
+              <label for="rota_inicio" class="col-md-3 control-label">Início do trajeto</label>
+              <div class="col-md-4">
+                <?php
+                $rotaInicio = new ModelEnderecos;
+                $inicio = $rotaInicio->getLogradouro($dataRota[0]['rota_saida']);
+                $inicio = $inicio[0]['logradouro_nome'] . ', ' . $inicio[0]['bairros_nome'] . ', ' . $inicio[0]['cidade_nome'] . ' - ' . $inicio[0]['cidade_uf'];
+                echo $inicio;
+                ?>
+              </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <label for="rota_fim" class="col-md-3 control-label">Fim do trajeto</label>
-            <div class="col-md-4">
-              <?php
-              $rotaFim = clone $rotaInicio;
-              $fim = $rotaFim->getLogradouro($dataRota[0]['rota_chegada']);
-              $fim = $fim[0]['logradouro_nome'] . ', ' . $fim[0]['bairros_nome'] . ', ' . $fim[0]['cidade_nome'] . ' - ' . $fim[0]['cidade_uf'];
-              echo $fim;
-              ?>
+            <div class="form-group">
+              <label for="rota_fim" class="col-md-3 control-label">Fim do trajeto</label>
+              <div class="col-md-4">
+                <?php
+                $rotaFim = clone $rotaInicio;
+                $fim = $rotaFim->getLogradouro($dataRota[0]['rota_chegada']);
+                $fim = $fim[0]['logradouro_nome'] . ', ' . $fim[0]['bairros_nome'] . ', ' . $fim[0]['cidade_nome'] . ' - ' . $fim[0]['cidade_uf'];
+                echo $fim;
+                ?>
+              </div>
             </div>
-          </div>
 
-          <hr />
+            <hr />
 
-          <div class="form-group">
-            <label for="" class="col-md-3 control-label">Escolas</label>
-            <div class="col-md-7">
-              <?php
-              $escolasCad = json_decode($dataRota[0]['rota_instituicoes'], true);
-              $escolasData = new ModelInstituicao;
+            <div class="form-group">
+              <label for="" class="col-md-3 control-label">Escolas</label>
+              <div class="col-md-7">
+                <?php
+                $escolasCad = json_decode($dataRota[0]['rota_instituicoes'], true);
+                $escolasData = new ModelInstituicao;
 
-              foreach($escolasCad as $idEscola){
-                $escola = clone $escolasData;
-                $escola->getInstituicao($idEscola);
-                if($escola->getResult()){
-                  $dataEscola = $escola->getResult();
-                  echo "<div class='row'>
-                  <ul>
-                  <li>Escola: {$dataEscola[0]['instituicao_nome']}</li>
-                  <li>Endereço: ".
-                  $dataEscola[0]['logradouro_nome'] . ', ' . $dataEscola[0]['instituicao_numero'] . ', '
-                  . $dataEscola[0]['bairros_nome'] . ', ' . $dataEscola[0]['cidade_nome'] . ' - '
-                  . $dataEscola[0]['cidade_uf']
-                  ."</li>
-                  </ul>
-                  </div>";
+                foreach($escolasCad as $idEscola){
+                  $escola = clone $escolasData;
+                  $escola->getInstituicao($idEscola);
+                  if($escola->getResult()){
+                    $dataEscola = $escola->getResult();
+                    echo "<div class='row'>
+                    <ul>
+                    <li>Escola: {$dataEscola[0]['instituicao_nome']}</li>
+                    <li>Endereço: ".
+                    $dataEscola[0]['logradouro_nome'] . ', ' . $dataEscola[0]['instituicao_numero'] . ', '
+                    . $dataEscola[0]['bairros_nome'] . ', ' . $dataEscola[0]['cidade_nome'] . ' - '
+                    . $dataEscola[0]['cidade_uf']
+                    ."</li>
+                    </ul>
+                    </div>";
+                  }
                 }
-              }
-              ?>
-            </div>
-          </div>
-
-          <hr />
-
-          <div class="form-group">
-            <label for="rota_veiculo" class="col-md-3 control-label">Veículo</label>
-            <div class="col-md-4">
-              <?php
-              $veiculos = new ModelVeiculo;
-              $veiculos->getLogradouro($dataRota[0]['rota_chegada']);
-              $fim = $fim[0]['logradouro_nome'] . ', ' . $fim[0]['bairros_nome'] . ', ' . $fim[0]['cidade_nome'] . ' - ' . $fim[0]['cidade_uf'];
-              echo $fim;
-              ?>
-            </div>
-          </div>
-
-          <hr />
-
-          <div class="form-group">
-            <label for="" class="col-md-3 control-label">Período</label>
-            <div class="col-md-8">
-              <div class="col-md-4">
-                <div class="input-group">
-                  <div class="input-group-addon">Início</div>
-                  <input type="date" name="inicio" class="form-control" placeholder="<?= date('d/m/Y'); ?>"
-                  value="<?= isset($dados['inicio']) ? date('d/m/Y', strtotime($dados['inicio'])) : date('d/m/Y', strtotime($dataRota[0]['rota_inicio'])); ?>" />
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="input-group">
-                  <div class="input-group-addon">Fim</div>
-                  <input type="date" name="fim" class="form-control" placeholder="<?= date('d/m/Y', strtotime('+1 year')); ?>"
-                  value="<?= isset($dados['fim']) ? date('d/m/Y', strtotime($dados['fim'])) : date('d/m/Y', strtotime($dataRota[0]['rota_fim'])); ?>" />
-                </div>
+                ?>
               </div>
             </div>
-          </div>
 
-          <hr />
+            <hr />
 
-          <div class="form-group">
-            <label for="observacoes" class="col-md-3 control-label">Observações</label>
-            <div class="col-md-4">
-              <textarea class="form-control" name="observacoes" id="observacoes" required="required"><?= isset($dados['observacoes']) ? $dados['observacoes'] : $dataRota[0]['rota_observacoes']; ?></textarea>
+            <div class="form-group">
+              <label for="rota_veiculo" class="col-md-3 control-label">Veículo</label>
+              <div class="col-md-4">
+                <?php
+                $veiculos = new ModelVeiculo;
+                $veiculos->getVeiculo($dataRota[0]['tb_veiculos_veiculo_id']);
+                $veiculo = $veiculos->getResult()[0]['veiculo_marca'] . '-' . $veiculos->getResult()[0]['veiculo_modelo'] .
+                ', Placa: ' . $veiculos->getResult()[0]['veiculo_placa'] . ' - ' .
+                (($veiculos->getResult()[0]['veiculo_agregado'] == 2) ? 'Veículo da Prefeitura.' : 'Veículo agregado.');
+                echo $veiculo;
+                ?>
+              </div>
             </div>
-          </div>
 
-          <hr />
+            <hr />
 
-          <div class="form-group">
-            <label for="" class="col-xs-3"></label>
-            <div class="col-xs-2">
-              <button type="submit" class="btn btn-success btn-block" name="editar"><i class="fa fa-download"></i> Atualizar</button>
+            <div class="form-group">
+              <label for="" class="col-md-3 control-label">Período</label>
+              <div class="col-md-8">
+                <div class="col-md-4">
+                  <div class="input-group">
+                    <div class="input-group-addon">Início</div>
+                    <input type="text" class="form-control" value="<?= date('d/m/Y', strtotime($dataRota[0]['rota_inicio'])); ?>" disabled="disabled" />
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="input-group">
+                    <div class="input-group-addon">Fim</div>
+                    <input type="text" class="form-control" value="<?= date('d/m/Y', strtotime($dataRota[0]['rota_fim'])); ?>" disabled="disabled" />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <?php
+
+            <hr />
+
+            <div class="form-group">
+              <label for="observacoes" class="col-md-3 control-label">Observações</label>
+              <div class="col-md-4">
+                <?= $dataRota[0]['rota_observacoes']; ?>
+              </div>
+            </div>
+
+            <hr />
+
+            <div class="form-group">
+              <label for="" class="col-xs-3"></label>
+              <div class="col-xs-2">
+                <button type="submit" class="btn btn-success btn-block" name="editar"><i class="fa fa-download"></i> Atualizar</button>
+              </div>
+            </div>
+            <?php
+          else:
+            ADSError('Não foram encontrados dados relacionados...', CRAZY_INFOR);
+          endif;
         else:
-          ADSError('Não foram encontrados dados relacionados...', CRAZY_INFOR);
+          ADSError('Parametro del nao encontrado', CRAZY_INFOR);
         endif;
-      else:
-        ADSError('Parametro del nao encontrado', CRAZY_INFOR);
-      endif;
-      ?>
+        ?>
+      </form>
     </div>
   </div>
 </div>
